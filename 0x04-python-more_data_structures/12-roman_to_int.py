10a910a658e98c5271d673cee8a192ffdbbca48d
Ahaneku_Laura <laurahaneku>
2022-07-07 03:24:51 -0700

    Updated 3 & 4

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-
-# roman_to_int - converts a roman numeral to an integer.
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
-	    'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-
-    if roman_string and type(roman_string) == str:
-        num = 0
-        for i in range(len(roman_string)):
-            for j in romans.keys():
-                if roman_string[:] is j:
-                    num = romans[j]
-                    return num
-                if roman_string[i:i+1] is j:
-                    num += romans[j]
-                    break
-        return num
+    ns = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if ns[roman_string[i]] >= ns[roman_string[i + 1]]:
+                total += ns[roman_string[i]]
+            else:
+                total -= ns[roman_string[i]]
+            i += 1
+        total += ns[roman_string[i]]
+        return total
     else:
         return 0
